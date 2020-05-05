4747fb302c4e418db0a9e2f33258877db655c06a
Felix Lamas <felamas@gmail.com>
2020-05-05 19:10:56 -0300

    subo la funcion de busqueda por id y por palabra

--- tpn4.cpp
+++ tpn4.cpp
@@ -12,12 +12,15 @@ typedef struct Tarea {
 void cargarTareas (Tarea **pTPendientes, int cantidadT);
 void controlarTareas(Tarea **pTPendientes, Tarea **pTRealizadas, int cantidad);
 void mostrar (Tarea **pTPendientes, int cantidad);
+void BusquedaPorId(Tarea **pTPendientes, Tarea **pTRealizadas, int cantidad, Tarea *busc);
+void BusqudeaPorId(Tarea **pTPendientes, Tarea **pTRealizadas, int cantidad, Tarea *busc);
 
 
 int main(int argc, char const *argv[])
 {
     int cantidadTareas;
     Tarea **TareasPendientes, **TareasRealizadas;
+    Tarea busc1, busc2;
 
     printf("Ingrese la cantidad de tareas a realizar:");
     scanf("%d",&cantidadTareas);
@@ -30,6 +33,9 @@ int main(int argc, char const *argv[])
     mostrar(TareasPendientes, cantidadTareas);
     printf("\nLas tareas realizadas son :\n");
     mostrar(TareasRealizadas, cantidadTareas);
+
+    BusquedaPorId(TareasPendientes, TareasRealizadas, cantidadTareas, &busc1);
+    BusqudeaPorId(TareasPendientes, TareasRealizadas, cantidadTareas, &busc2);
     free(*TareasPendientes);
     free(*TareasRealizadas);
     return 0;
@@ -75,6 +81,7 @@ void controlarTareas(Tarea **pTPendientes, Tarea **pTRealizadas, int cantidad)
     }
     printf("Se controlaron todas las tareas\n");
 }
+
 void mostrar(Tarea **pTPendientes, int cantidad)
 {
     for (int i = 0; i < cantidad; i++)
@@ -91,3 +98,56 @@ void mostrar(Tarea **pTPendientes, int cantidad)
     }
 }
 
+void BusquedaPorId(Tarea **pTPendientes, Tarea **pTRealizadas, int cantidad, Tarea *busc)
+{
+    int id;
+    printf("ingrese el ID de la tarea a buscar (no debe mayor que %d):", cantidad);
+    scanf("%d", &id);
+    getchar();
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (pTPendientes[i] && pTPendientes[i]->TareaID == id)
+        {
+            printf("La tarea con ID %d esta pendiende\n", id);
+            busc=pTPendientes[i];
+        }
+        else
+        {
+            if (pTRealizadas[i] && pTRealizadas[i]->TareaID == id)
+            {
+                printf("La tarea con ID %d esta realizada\n", id);
+                busc=pTRealizadas[i];
+            }
+            
+        }
+        
+        
+    }   
+}
+
+void BusqudeaPorId(Tarea **pTPendientes, Tarea **pTRealizadas, int cantidad, Tarea *busc)
+{
+    char busqueda[TAMANIO];
+    printf("Ingrese una palabra clave para la busqueda de la tarea:");
+    gets(busqueda);
+    getchar();
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (pTPendientes[i] && strcmp(pTPendientes[i]->Descripcion,busqueda) == 0)
+        {
+            printf("La tarea esta pendiende\n");
+            busc=pTPendientes[i];
+        }
+        else
+        {
+            if (pTRealizadas[i] && strcmp(pTRealizadas[i]->Descripcion,busqueda) == 0)
+            {
+                printf("La tarea esta realizada\n");
+                busc=pTRealizadas[i];
+            }
+            
+        }
+        
+        
+    }   
+}
